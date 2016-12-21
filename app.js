9bcb820ba12348b9e04ec3115a07241059235514
Richard Key <rich@busyrich.com>
2016-12-21 12:17:45 -0800

    Added a start message to server start.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var static = require('node-static');
+var static = require('node-static'),
+		file = new static.Server('./public') //Creates a static file server from the ./public folder
+		port = 5000;
 
-//
-// Create a node-static server instance to serve the './public' folder
-//
-var file = new static.Server('./public');
-require('http').createServer(function (request, response) {
+var server = require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         //
         // Serve files!
         //
         file.serve(request, response);
     }).resume();
-}).listen(5000);
+}).listen(port, function() {
+	console.log('Server Started on port %s', port);
+});
