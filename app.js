9ffc0a9bd175cd5055ea983d3a1b35f818533cb6
Richard Key <rich@busyrich.com>
2016-12-20 21:58:37 -0800

    Refactored the project to run off a simple node-static server instead of app.js

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
-var app = module.exports = require('appjs');
+var static = require('node-static');
 
-app.serveFilesFrom(__dirname + '/content');
-
-var window = app.createWindow(
-{
-	width	: 1280,
-	height : 720,
-	icons	: __dirname + '/content/icons'
-});
-
-window.on('create', function()
-{
-	window.frame.show();
-	window.frame.center();
-});
-
-window.on('ready', function()
-{
-    window.require = require;
-	window.process = process;
-	window.module = module;
-
-	function F12(e) { return e.keyIdentifier === 'F12' }
-	function Command_Option_J(e) { return e.keyCode === 74 && e.metaKey && e.altKey }
-
-	window.addEventListener('keydown', function(e)
-	{
-		if (F12(e) || Command_Option_J(e))
-			window.frame.openDevTools();
-	});
-
-    window.dispatchEvent(new window.Event('app-ready'));
-});
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('./public');
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(5000);
