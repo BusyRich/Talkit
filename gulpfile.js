a5ba543a292f1dcd02ac7c20fb1850526b58fecc
Richard Key <rich@busyrich.com>
2016-12-25 21:20:08 -0800

    Added SASS compliation to the build process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),  
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  return gulp.src(['public/js/ditree.nodes.js','public/js/ditree.app.js'])
+gulp.task('default', ['scripts', 'sass']);
+
+var scriptsDir = 'public/js',
+    scriptFiles = [
+      scriptsDir + '/ditree.nodes.js',
+      scriptsDir + '/ditree.app.js'
+    ];
+
+gulp.task('scripts', function() {
+  return gulp.src(scriptFiles)
     .pipe(concat('ditree.js'))
-    .pipe(gulp.dest('public/js'))
+    .pipe(gulp.dest(scriptsDir))
     .pipe(rename('ditree.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('public/js'));
+    .pipe(gulp.dest(scriptsDir));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('public/css/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename('ditree.min.css'))
+    .pipe(gulp.dest('public/css'));
 });
